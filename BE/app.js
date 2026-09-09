bfde86c89c0c93a264f0e6cd51f308ed9082f5b2
Divyesh Kakadiya <divyeshkfc212@gmail.com>
2026-09-09 18:18:55 +0530

    feat: initialize Express application with middleware, CORS, and Swagger API documentation

--- BE/app.js
+++ BE/app.js
@@ -17,7 +17,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:4200",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: false,
   }),
